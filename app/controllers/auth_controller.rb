e191b0049ededc33742a0c6466257c3607e85c9a
Tanuka16 <tanuka.das12@gmail.com>
2020-04-10 17:49:22 -0400

    fixed the authentication error

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,12 +1,20 @@
 class AuthController < ApplicationController
   def create
-    user = User.find_by_email(params[:email])
+    user = User.find_by(email: params[:email].downcase)
     is_authenticated = user.authenticate(params[:password])
 
     if is_authenticated
       render json: user
     else
-      render json: {errors: ["Wrong email id or password."]}, status: :unprocessable_entity
+      render json: {errors: ["Invalid email id or password"]}, status: 422
     end
   end
 end
+
+#
+
+#   if user && user.authenticate(params[:password])
+#     render json: user
+#   else
+#     render json: {errors: ["Wrong email id or password."]}, status: 422
+#   end
